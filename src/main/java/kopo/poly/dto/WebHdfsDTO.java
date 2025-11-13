a09e96aee957d0076a28aaf89c03fbadf334252d
kimyega <kimyega@naver.com>
2025-11-13 17:10:45 +0900

    hadoop

--- src/main/java/kopo/poly/dto/WebHdfsDTO.java
+++ src/main/java/kopo/poly/dto/WebHdfsDTO.java
@@ -1,4 +1,14 @@
 package kopo.poly.dto;
 
+import lombok.*;
+
+@Data
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class WebHdfsDTO {
+
+    private String path;
+    private String content;
 }
