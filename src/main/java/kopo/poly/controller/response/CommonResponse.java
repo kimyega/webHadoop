44f21803a94268540e794396275faa55aed42d06
kimyega <kimyega@naver.com>
2025-11-13 17:10:01 +0900

    hadoop

--- src/main/java/kopo/poly/controller/response/CommonResponse.java
+++ src/main/java/kopo/poly/controller/response/CommonResponse.java
@@ -1,4 +1,25 @@
 package kopo.poly.controller.response;
 
-public class CommonResponse {
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+@Data
+public class CommonResponse<T> {
+
+    private HttpStatus httpStatus;
+    private String message;
+    private T data;
+
+    @Builder
+    public CommonResponse(HttpStatus httpStatus, String message, T data) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> CommonResponse<T> of(HttpStatus httpStatus, String message, T data) {
+        return new CommonResponse<>(httpStatus, message, data);
+    }
+
 }
