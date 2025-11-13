e35bd505b18d99f62f29390fc4bd98399469bd42
kimyega <kimyega@naver.com>
2025-11-13 17:10:10 +0900

    hadoop

--- src/main/java/kopo/poly/service/IWebHdfsService.java
+++ src/main/java/kopo/poly/service/IWebHdfsService.java
@@ -1,4 +1,15 @@
 package kopo.poly.service;
 
+import kopo.poly.dto.WebHdfsDTO;
+
 public interface IWebHdfsService {
+
+    String HDFS_URI = "http://192.168.48.129:9870/webhdfs/v1";
+    String USER_NAME = "hadoop";
+
+    String upload(WebHdfsDTO pDTO);
+
+    String delete(WebHdfsDTO pDTO);
+
+    String list(WebHdfsDTO pDTO);
 }
